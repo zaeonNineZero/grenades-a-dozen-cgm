a8a9826e60b55696ce1abf0b06236c604fb2c76d
zaeonNineZero <zaeonnine0@gmail.com>
2025-06-30 18:31:47 -0400

    * Fixed a half baked mixin setup so mixins ACTUALLY work now. This means the Smoked effect now blocks the screen!

--- src/main/java/zaeonninezero/grenadesadozen/mixin/MixinPlugin.java
+++ src/main/java/zaeonninezero/grenadesadozen/mixin/MixinPlugin.java
@@ -1,4 +1,4 @@
-package zaeonninezero.grenadesadozen.guns.mixin;
+package zaeonninezero.grenadesadozen.mixin;
 
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
